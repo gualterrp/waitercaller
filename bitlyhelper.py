95569155bec8f6c651e9201be50c3cae91eef400
Gualter Portella <gualterrp@gmail.com>
2017-06-03 19:58:42 -0300

    finished forms

---
+++ bitlyhelper.py
@@ -0,0 +1,18 @@
+import urllib2
+import json
+
+TOKEN = "e1a3fc0223b65509b09fcc74802ff8bcb13ca41c"
+ROOT_URL = "https://api-ssl.bitly.com"
+SHORTEN = "/v3/shorten?access_token={}&longUrl={}"
+
+
+class BitlyHelper:
+
+    def shorten_url(self, longurl):
+        try:
+            url = ROOT_URL + SHORTEN.format(TOKEN, longurl)
+            response = urllib2.urlopen(url).read()
+            jr = json.loads(response)
+            return jr['data']['url']
+        except Exception as e:
+            print e
--- passwordhelper.py
+++ passwordhelper.py
@@ -3,12 +3,12 @@ import os
 import base64
 
 class PasswordHelper:
-    
-    def get_hash(self, plain):
-        return hashlib.sha512(plain).hexdigest()
 
-    def get_salt(self):
-        return base64.b64encode(os.urandom(20))
+	def get_hash(self, plain):
+		return hashlib.sha512(plain).hexdigest()
 
-    def validate_password(self, plain, salt, expected):
-        return self.get_hash(plain + salt) == expected
+	def get_salt(self):
+		return base64.b64encode(os.urandom(20))
+
+	def validate_password(self, plain, salt, expected):
+		return self.get_hash(plain + salt) == expected
